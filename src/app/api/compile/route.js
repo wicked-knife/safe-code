fdf71dad35d1a80d01e5a424b483a51514c65846
fenggang <fenggang@chongho.net>
2023-10-09 17:22:30 +0800

    add some static styles

--- src/app/api/compile/route.js
+++ src/app/api/compile/route.js
@@ -1,5 +1,5 @@
 import {NextResponse} from 'next/server'
 
 export async function GET(request, response) {
-  return NextResponse.json({hello: 'world'})
+  return NextResponse.json({hello: 'world', path: request.nextUrl.pathname})
 }
